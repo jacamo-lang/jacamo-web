237dad061178badba622cf7f4788c94f1837e115
Cleber <cleberjamaral@gmail.com>
2018-11-21 16:09:18 -0200

    Now top buttons are contracting properly

--- src/java/jacamo/web/RestImpl.java
+++ src/java/jacamo/web/RestImpl.java
@@ -44,38 +44,38 @@ public class RestImpl extends AbstractBinder {
         so.append("		<div id=\"root\">\n"); 
         so.append("			<header class=\"row\">\n");
         // logo JaCaMo
-        so.append("				<span class=\"logo col-sm-2 col-md\">JaCaMo</span>\n"); 
+        so.append("				<span class=\"logo col-xp-1 col-sm-2 col-md\">JaCaMo</span>\n"); 
         // top menu - button agents
-        so.append("				<a class=\"button col-sm col-md\" href=\"agents/\" target=\"mainframe\">\n" +
+        so.append("				<a class=\"button col-xp-1 col-sm-2 col-md\" href=\"agents/\" target=\"mainframe\">\n" +
                   "					<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"\n" + 
                   "						fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\"\n" + 
                   "						stroke-linejoin=\"round\" style=\"height: 20px; vertical-align: text-top;\">\n" + 
                   "						<circle cx=\"12\" cy=\"12\" r=\"11\"/>\n" + 
                   "					</svg><span>&nbsp;Agents</span>\n" + 
                   "				</a>\n");
-        so.append("				<a class=\"button col-sm col-md\" href=\"workspaces/\" target=\"mainframe\">\n" + 
+        so.append("				<a class=\"button col-xp-1 col-sm-2 col-md\" href=\"workspaces/\" target=\"mainframe\">\n" + 
                   "					<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"\n" +  
                   "						fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\"\n" + 
                   "						stroke-linejoin=\"round\" style=\"height: 20px; vertical-align: text-top;\">\n" + 
                   "						<polygon points=\"0 1, 24 1, 24 8, 0 8, 0 16, 24 16, 24 23, 0 23, 0 1, 24 1, 24 23, 0 23\"></polygon>\n" +
                   "					</svg><span>&nbsp;Environment</span>\n" +
                   "				</a>\n");
-        so.append("				<a class=\"button col-sm col-md\" href=\"oe/\" target=\"mainframe\">\n" + 
+        so.append("				<a class=\"button col-xp-1 col-sm-2 col-md\" href=\"oe/\" target=\"mainframe\">\n" + 
                   "					<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"\n" + 
                   "						fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\"\n" + 
                   "						stroke-linejoin=\"round\" style=\"height: 20px; vertical-align: text-top;\">\n" + 
                   "						<polygon points=\"0 1, 10 1, 10 6, 24 6, 24 23, 0 23, 0 6, 10 6, 0 6, 0 1\"></polygon>\n" + 
                   "					</svg><span>&nbsp;Organisation</span>\n" + 
                   "				</a>\n");
-        so.append("               <label for=\"doc-drawer-checkbox\" class=\"button drawer-toggle\"></label>\n");
-        so.append("               <input id=\"doc-drawer-checkbox\" class=\"drawer\" value=\"on\" type=\"checkbox\">\n" + 
-                  "               <nav class=\"col-md-2 col-lg-2\" id=\"nav-drawer\">\n" + 
-                  "                   <label for=\"doc-drawer-checkbox\" class=\"button drawer-close\"></label>\n" + 
-                  "                   <h3>Menu</h3>\n" + 
-                  "                   <a hef=\"agents/\">Agents</a>\n" + 
-                  "                   <a hef=\"workspaces/\">Environment</a>\n" + 
-                  "                   <a hef=\"oe/\">Organisation</a>\n" + 
-                "               </nav>\n");
+        so.append("				<label for=\"doc-drawer-checkbox\" class=\"button drawer-toggle\"></label>\n");
+        so.append("				<input id=\"doc-drawer-checkbox\" class=\"drawer\" value=\"on\" type=\"checkbox\">\n" + 
+                  "				<nav class=\"col-xp-1 col-md-2\" id=\"nav-drawer\">\n" + 
+                  "					<label for=\"doc-drawer-checkbox\" class=\"button drawer-close\"></label>\n" + 
+                  "					<h3>Menu</h3>\n" + 
+                  "					<a hef=\"agents/\">Agents</a>\n" + 
+                  "					<a hef=\"workspaces/\">Environment</a>\n" + 
+                  "					<a hef=\"oe/\">Organisation</a>\n" + 
+                  "				</nav>\n");
         so.append("			</header>\n"); 
         so.append("			<div class=\"second-row\" id=\"full-content\">\n");
         so.append("				<iframe id=\"mainframe\" name=\"mainframe\" width=\"100%\" height=\"100%\"\n" + 
