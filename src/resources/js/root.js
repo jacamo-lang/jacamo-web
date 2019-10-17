4d1bae27f8172d5a1fa79c743346047269ae8422
cleberjamaral <cleberjamaral@gmail.com>
2019-10-17 07:39:59 -0300

    Working of whole MAS overview

--- src/resources/js/root.js
+++ src/resources/js/root.js
@@ -1,3 +1,6 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+NEW ORGANIZATIONAL ROLE
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 /* create role: POST in "/{oename}/group/{groupname}" */
 function newRole(org, gr) {
@@ -27,3 +30,90 @@ function newRole(org, gr) {
     http.send(data);
     window.open('/oe/' + org + '/os', 'mainframe', 'location=yes,status=yes');
 }
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+WHOLE SYSTEM AS DOT
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/*Get list of agent from backend*/
+var getAgents = new Promise((res, rej) => {
+  var agents = ["agent1", "agent2"];
+  const Http = new XMLHttpRequest();
+  Http.open("GET", "./agents");
+  Http.send();
+  Http.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      res(JSON.parse(Http.responseText));
+    }
+  };
+});
+
+/*Get one agent from backend*/
+function getAgent(agent) {
+	return new Promise(function(res, rej) {
+		const Http = new XMLHttpRequest();
+	  Http.open("GET", "/agents/" + agent);
+		Http.send();
+		Http.onreadystatechange = function() {
+			console.log("Trying to acces " + "/agents/" + agent);
+	    if (this.readyState == 4 && this.status == 200) {
+				res(JSON.parse(Http.responseText));
+	    }
+	  };
+	});
+};
+
+
+function getMASAsDot() {
+	const MAX_LENGTH = 35;
+  var dot = [];
+  var allwks = [];
+
+  dot.push("digraph G {\n");
+  dot.push("\tgraph [\n");
+  dot.push("\t\trankdir=\"TB\"\n");
+  dot.push("\t\tbgcolor=\"transparent\"\n");
+  dot.push("\t]\n");
+
+	{
+	  /* agents dimension */
+	  dot.push("\tsubgraph cluster_ag {\n");
+	  dot.push("\t\tlabel=\"agents\"\n");
+	  dot.push("\t\tlabeljust=\"r\"\n");
+	  dot.push("\t\tpencolor=gray\n");
+	  dot.push("\t\tfontcolor=gray\n");
+	  {
+	    /* agent's mind */
+			getAgents.then((agents) => {agents.forEach(function(a) {
+				var s1 = (a.length <= MAX_LENGTH) ? a : a.substring(0, MAX_LENGTH) + " ...";
+	      dot.push("\t\t\"" + a + "\" [ ");
+	      dot.push("\n\t\t\tlabel = \"" + s1 + "\"");
+	      dot.push("\n\t\t\tshape = \"ellipse\" style=filled fillcolor=white\n");
+	      dot.push("\t\t];\n");
+	      /* get workspaces the agent are in (including organizations) */
+	      var workspacesIn = [];
+				getAgent(a).then((ag) => {
+					ag.workspaces.forEach(function(w) {
+						workspacesIn.push(w.workspace);
+					});
+				});
+	      allwks.push(workspacesIn);
+				dot.push("\t\t{rank=same " +
+		      agents +
+		      "};\n");
+		    dot.push("\t};\n");
+			})});
+	  }
+	}
+
+
+
+
+
+  dot.push("}\n");
+	console.log(dot);
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+END OF FILE
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
