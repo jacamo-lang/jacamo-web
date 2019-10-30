522098bd298a14dafc30f8de570ad60ca84cd731
cleberjamaral <cleberjamaral@gmail.com>
2019-10-30 01:38:23 -0300

    Unifying code, removed oe_common.js

--- src/resources/js/root.js
+++ src/resources/js/root.js
@@ -531,8 +531,6 @@ function updateWorkspaceMenu(nav, ws, ar, addCloseButton) {
   });
 }
 
-
-
 function getWorkspaceDetails() {
   const params = new URL(location.href).searchParams;
   const selectedWorkspace = params.get('workspace');
@@ -641,6 +639,69 @@ function setWorkspaceModalWindow() {
     }
   };
 }
+
+/**
+ * ORGANISATION FUNCTIONS
+ */
+
+ /*Get Organisations */
+ function getOE() {
+   get("./oe").then(function(resp){
+     updateOrganisationMenu("nav-drawer", JSON.parse(resp), true);
+     updateOrganisationMenu("nav-drawer-frame", JSON.parse(resp), false);
+   });
+ };
+
+ function updateOrganisationMenu(nav, set, addCloseButton) {
+   if (addCloseButton) {
+     const closeButton = document.createElement('label');
+     closeButton.setAttribute("for","doc-drawer-checkbox");
+     closeButton.setAttribute("class","button drawer-close");
+     document.getElementById(nav).appendChild(closeButton);
+     var h3 = document.createElement("h3");
+     h3.innerHTML = "&#160";
+     document.getElementById(nav).appendChild(h3);
+   }
+
+   const selectedItem = new URL(location.href).searchParams.get('organisation');
+   set.sort();
+   set.forEach(function(n) {
+     var lag = document.createElement('a');
+     lag.setAttribute("href", "./organisation.html?organisation=" + n);
+     if (selectedItem === n) {
+       lag.innerHTML = "<h5><b>" + n + "</b></h5>";
+       document.getElementById(nav).appendChild(lag);
+     } else {
+       lag.innerHTML = "<h5>" + n + "</h5>";
+       document.getElementById(nav).appendChild(lag);
+     }
+   });
+   var br = document.createElement("br");
+   document.getElementById(nav).appendChild(br);
+   document.getElementById(nav).appendChild(br);
+   var lnew = document.createElement('a');
+   lnew.setAttribute("href", "./oe_role_new.html");
+   lnew.innerHTML = "create role";
+   document.getElementById(nav).appendChild(lnew);
+ }
+
+ /* create role: POST in "/{oename}/group/{groupname}" */
+ function newRole(org, gr) {
+   http = new XMLHttpRequest();
+   var input = document.getElementById('orgGrRole').value;
+   var lastDot = input.lastIndexOf('.');
+   var firstPart = input.substring(0, lastDot);
+   var role = input.substring(lastDot + 1);
+   var firstDot = firstPart.indexOf('.');
+   var org = firstPart.substring(0, firstDot);
+   var group = firstPart.substring(firstDot + 1);
+
+   http.open("POST", '/oe/' + org + '/group/' + group, false);
+   http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+   var data = "role=" + encodeURIComponent(role);
+   http.send(data);
+   window.location.assign('/oe.html');
+ }
 
 /**
  * END OF FILE
