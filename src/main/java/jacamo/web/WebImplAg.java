cda1e62fa1139e4653810ef09a3c80a7791db164
cleberjamaral <cleberjamaral@gmail.com>
2020-04-04 01:07:12 -0300

    Moved /code from REST

--- src/main/java/jacamo/web/WebImplAg.java
+++ src/main/java/jacamo/web/WebImplAg.java
@@ -10,6 +10,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 import javax.inject.Singleton;
 import javax.ws.rs.Consumes;
@@ -24,11 +28,16 @@ import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.media.multipart.FormDataParam;
 
+import com.google.common.reflect.ClassPath;
+import com.google.common.reflect.ClassPath.ClassInfo;
 import com.google.gson.Gson;
 
 import cartago.ArtifactId;
+import cartago.ArtifactInfo;
 import cartago.CartagoException;
 import cartago.CartagoService;
+import cartago.WorkspaceId;
+import jaca.CAgentArch;
 import jacamo.infra.JaCaMoLauncher;
 import jacamo.rest.RestImplAg;
 import jacamo.rest.TranslEnv;
@@ -44,10 +53,12 @@ import jason.asSyntax.Plan;
 import jason.asSyntax.PlanBody;
 import jason.asSyntax.PlanLibrary;
 import jason.asSyntax.Term;
+import jason.asSyntax.Trigger.TEType;
 import jason.asSyntax.parser.ParseException;
 import jason.asSyntax.parser.as2j;
 import jason.infra.centralised.BaseCentralisedMAS;
 import jason.infra.centralised.CentralisedAgArch;
+import jason.stdlib.print;
 
 /**
  * Agent's REST implementation class
@@ -282,6 +293,160 @@ public class WebImplAg extends RestImplAg { // TODO: replace by extends RestImpl
         }
     }
 
+    /**
+     * Return a TEXT PLAIN of available internal action, external actions and
+     * commands for the given agent Example:
+     * "['.desire','.drop_desire','.drop_all_desires']"
+     * 
+     * @param agName Name of the agent
+     * @return HTTP 200 Response (ok status) or 500 Internal Server Error in case of
+     *         error (based on https://tools.ietf.org/html/rfc7231#section-6.6.1)
+     */
+    @Path("/{agentname}/code")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCodeCompletionSuggestions(@PathParam("agentname") String agName) {
+        Map<String,String> commands = new HashMap<>();
+        try {
+            // get internal actions
+            getPlansSuggestions(agName, commands);
+            // get internal actions
+            getIASuggestions(commands);
+            // get external actions
+            getEASuggestions(agName, commands);
+
+            Gson json = new Gson();
+            Map<String,String> sortedCmds = new TreeMap<>(commands);
+            return Response.ok(json.toJson(sortedCmds)).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return Response.status(500, "Server Internal Error! Could not get code completion suggestions.").build();
+    }
+    
+    public void getPlansSuggestions(String agName, Map<String, String> commands) {
+        try {
+            // get agent's plans
+            Agent ag = getAgent(agName);
+            if (ag != null) {
+                PlanLibrary pl = ag.getPL();
+                for (Plan plan : pl.getPlans()) {
+                    
+                    // do not add plans that comes from jar files (usually system's plans)
+                    if (plan.getSource().startsWith("jar:file") || plan.getSource().equals("kqmlPlans.asl"))
+                        continue;
+
+                    // add namespace when it is not default
+                    String ns = "";
+                    if (!plan.getNS().equals(Literal.DefaultNS)) {
+                        ns = plan.getNS().toString() + "::";
+                    }
+
+                    String terms = "";
+                    if (plan.getTrigger().getLiteral().getArity() > 0) {
+                        for (int i = 0; i < plan.getTrigger().getLiteral().getArity(); i++) {
+                            if (i == 0)
+                                terms = "(";
+                            terms += plan.getTrigger().getLiteral().getTerm(i).toString();
+                            if (i < plan.getTrigger().getLiteral().getArity() - 1)
+                                terms += ", ";
+                            else
+                                terms += ")";
+                        }
+                    }
+
+                    // when it is a goal or test goal, do not add operator
+                    if ((plan.getTrigger().getType() == TEType.achieve)
+                            || (plan.getTrigger().getType() == TEType.test)) {
+
+
+                        commands.put(ns + plan.getTrigger().getType().toString()
+                        + plan.getTrigger().getLiteral().getFunctor() + terms, "");
+
+                    }
+                    // when it is belief, do not add type which is anyway empty
+                    else if (plan.getTrigger().getType() == TEType.belief) {
+                        commands.put(ns + plan.getTrigger().getOperator().toString()
+                                + plan.getTrigger().getLiteral().getFunctor() + terms, "");
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getEASuggestions(String agName, Map<String, String> commands) throws CartagoException {
+        try {
+            Agent ag = getAgent(agName);
+            // get external actions (from focused artifacts)
+            CAgentArch cartagoAgArch = getCartagoArch(ag);
+            for (WorkspaceId wid : cartagoAgArch.getSession().getJoinedWorkspaces()) {
+                String wksName = wid.getName();
+                for (ArtifactId aid : CartagoService.getController(wksName).getCurrentArtifacts()) {
+
+                    // operations
+                    ArtifactInfo info = CartagoService.getController(wksName).getArtifactInfo(aid.getName());
+
+                    info.getObservers().forEach(y -> {
+                        if (y.getAgentId().getAgentName().equals(agName)) {
+                            info.getOperations().forEach(z -> {
+                                String params = "";
+                                for (int i = 0; i < z.getOp().getNumParameters(); i++) {
+                                    if (i == 0) params = "(";
+                                    params += "arg" + i;
+                                    if (i == z.getOp().getNumParameters() - 1)
+                                        params += ")";
+                                    else
+                                        params += ", ";
+                                }
+
+                                commands.put(z.getOp().getName() + params, "");
+                            });
+                        }
+                    });
+
+                }
+            }
+        } catch (CartagoException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Get list of internal actions for an agent
+     * 
+     * @return List of internal actions
+     */
+    public void getIASuggestions(Map<String,String> cmds) {
+        try {
+            ClassPath classPath = ClassPath.from(print.class.getClassLoader());
+            Set<ClassInfo> allClasses = classPath.getTopLevelClassesRecursive("jason.stdlib");
+
+            allClasses.forEach(a -> {
+                try {
+                    Class<?> c = a.load();
+                    if (c.isAnnotationPresent(jason.stdlib.Manual.class)) {
+                        // add full predicate provided by @Manual
+                        jason.stdlib.Manual annotation = (jason.stdlib.Manual) c
+                                .getAnnotation(jason.stdlib.Manual.class);
+                        cmds.put(annotation.literal(), annotation.hint().replaceAll("\"", "`").replaceAll("'", "`"));
+					} else {
+						// add just the functor of the internal action
+						cmds.put("." + a.getSimpleName(), "");
+					}
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			});
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+    
     private void lookForCollaborativeExceptions(Agent ag) throws UnderstandabilityException, UsefulnessException {
         PlanLibrary pl = ag.getPL();
         for (Plan plan : pl.getPlans()) {
