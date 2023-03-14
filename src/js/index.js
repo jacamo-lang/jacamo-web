b316dc0211f63bdc683094a94f1fddc13354acc3
cleberjamaral <cleberjamaral@gmail.com>
2023-03-14 20:12:41 -0300

    Fixed overview graph for rest 0.7

--- src/js/index.js
+++ src/js/index.js
@@ -129,89 +129,101 @@ const g = require('./git')
    });
  }
 
-/* WHOLE SYSTEM AS DOT */
+let masGraphCache = undefined;
+ /* WHOLE SYSTEM AS DOT */
 function getMASAsDot() {
   let dot = [];
-
+  
   h.get('./overview').then(function(mas) {
     let overview = JSON.parse(mas);
 
-    dot.push("digraph G { graph [ rankdir=\"TB\" bgcolor=\"transparent\"]\n");
+    if (overview.agents.length === 0) {
+      dot = ["digraph G { graph [ rankdir=\"TB\" bgcolor=\"transparent\"]\n noAg [label=<There are<br />no agents>]\n}\n"];
+    } else {
+      dot.push("digraph G { graph [ rankdir=\"TB\" bgcolor=\"transparent\"]\n");
 
-    /* Organisation dimension */
-    dot.push("\tsubgraph cluster_org {\n");
-    dot.push("\t\tlabel=\"organisation\" labeljust=\"r\" pencolor=gray fontcolor=gray\n");
-    overview.organisations.forEach(function(o) {
-      dot.push("\t\tsubgraph cluster_" + o.organisation + " {\n");
-      dot.push("\t\t\tlabel=\"" + o.organisation + "\" labeljust=\"r\" pencolor=gray40 fontcolor=gray40\n");
-      o.groups.forEach(function(g) {
-        dot.push("\t\t\t\"" + g.id + "\" [ " + "label = \"" + g.id + "\" shape=tab style=filled pencolor=black fillcolor=lightgrey];\n");
-      });
-      dot.push("\t\t}\n");
-    });
-
-    let orglinks = [];
-    overview.agents.forEach(function(a) {
-      a.roles.forEach(function(r) {
-        orglinks.push("\t\"" + r.group + "\"->\"" + a.agent + "\" [arrowtail=normal dir=back label=\"" + r.role + "\"]\n");
-      });
-    });
-    dot.push("\t}\n");
-    dot.push(orglinks.join(" "));
-
-    /* agents dimension */
-    dot.push("\tsubgraph cluster_ag {\n");
-    dot.push("\t\tlabel=\"agents\" labeljust=\"r\" pencolor=gray fontcolor=gray\n");
-    let ags = [];
-    overview.agents.forEach(function(x) {
-      ags.push(x.agent);
-      var s1 = (x.agent.length <= p.MAX_LENGTH) ? x.agent : x.agent.substring(0, p.MAX_LENGTH) + " ...";
-      dot.push("\t\t\"" + x.agent + "\" [label = \"" + s1 + "\" shape = \"ellipse\" style=filled fillcolor=white];\n");
-    });
-    dot.push("\t}\n");
-
-    /* Environment dimension */
-    dot.push("\tsubgraph cluster_env {\n");
-    dot.push("\t\tlabel=\"environment\" labeljust=\"r\" pencolor=gray fontcolor=gray\n");
-    overview.agents.forEach(function(ag) {
-      let wksartifacts = [];
-      if (Object.keys(ag.workspaces).length > 0) {
-        Object.keys(ag.workspaces).forEach(function(w) {
-          let envlinks = [];
-          let wks = ag.workspaces[w];
-          let wksName = wks.workspace;
-          dot.push("\t\tsubgraph cluster_" + wksName + " {\n");
-          dot.push("\t\t\tlabel=\"" + wksName + "\" labeljust=\"r\" style=dashed pencolor=gray40 fontcolor=gray40\n");
-          Object.keys(wks.artifacts).forEach(function(a) {
-            if (p.HIDDEN_ARTS.indexOf(wks.artifacts[a].type) < 0) {
-              s1 = (wks.artifacts[a].artifact.length <= p.MAX_LENGTH) ? wks.artifacts[a].artifact :
-                wks.artifacts[a].artifact.substring(0, p.MAX_LENGTH) + " ...";
-              dot.push("\t\t\t\"" + wksName + "_" + wks.artifacts[a].artifact + "\" [label = \"" + s1 + ":\\n");
-              s1 = (wks.artifacts[a].type.length <= p.MAX_LENGTH) ? wks.artifacts[a].type :
-                wks.artifacts[a].type.substring(0, p.MAX_LENGTH) + " ...";
-              dot.push(s1 + "\"shape=record style=filled fillcolor=white];\n");
-              wksartifacts.push(wksName + "_" + wks.artifacts[a].artifact);
-              envlinks.push("\t\t\"" + ag.agent + "\"->\"" + wksName + "_" + wks.artifacts[a].artifact + "\" [arrowhead=odot]\n");
-            }
-          });
-
-          dot.push("\t\t}\n");
-          dot.push(envlinks.join(" "));
+      /* Organisation dimension */
+      dot.push("\tsubgraph cluster_org {\n");
+      dot.push("\t\tlabel=\"organisation\" labeljust=\"r\" pencolor=gray fontcolor=gray\n");
+      overview.organisations.forEach(function(o) {
+        dot.push("\t\tsubgraph cluster_" + o.organisation + " {\n");
+        dot.push("\t\t\tlabel=\"" + o.organisation + "\" labeljust=\"r\" pencolor=gray40 fontcolor=gray40\n");
+        o.groups.forEach(function(g) {
+          dot.push("\t\t\t\"" + g.id + "\" [ " + "label = \"" + g.id + "\" shape=tab style=filled pencolor=black fillcolor=lightgrey];\n");
         });
-      }
-    });
-    dot.push("\t}\n");
-
-    dot.push("}\n");
-
-    /* Transition follows modal top down movement */
-    import( /* webpackChunkName: "d3" */ 'd3').then(function(d3) {
-      import( /* webpackChunkName: "d3-graphviz" */ 'd3-graphviz').then(function(d3G) {
-        var t = d3.transition().duration(750).ease(d3.easeLinear);
-        if (overview.agents.length === 0) dot = ["digraph G { graph [ rankdir=\"TB\" bgcolor=\"transparent\"]\n noAg [label=<There is<br />no agents>]\n}\n"];
-        d3G.graphviz("#overviewgraph").transition(t).renderDot(dot.join(""));
+        dot.push("\t\t}\n");
       });
-    });
+  
+      let orglinks = [];
+      overview.agents.forEach(function(a) {
+        a.roles.forEach(function(r) {
+          orglinks.push("\t\"" + r.group.value + "\"->\"" + a.agent.value + "\" [arrowtail=normal dir=back label=\"" + r.role.value + "\"]\n");
+        });
+      });
+      dot.push("\t}\n");
+      dot.push(orglinks.join(" "));
+  
+      /* agents dimension */
+      dot.push("\tsubgraph cluster_ag {\n");
+      dot.push("\t\tlabel=\"agents\" labeljust=\"r\" pencolor=gray fontcolor=gray\n");
+      let ags = [];
+      overview.agents.forEach(function(x) {
+        ags.push(x.agent);
+        var s1 = (x.agent.value.length <= p.MAX_LENGTH) ? x.agent.value : x.agent.value.substring(0, p.MAX_LENGTH) + " ...";
+        dot.push("\t\t\"" + x.agent.value + "\" [label = \"" + s1 + "\" shape = \"ellipse\" style=filled fillcolor=white];\n");
+      });
+      dot.push("\t}\n");
+  
+      /* Environment dimension */
+      dot.push("\tsubgraph cluster_env {\n");
+      dot.push("\t\tlabel=\"environment\" labeljust=\"r\" pencolor=gray fontcolor=gray\n");
+      overview.agents.forEach(function(ag) {
+        let wksartifacts = [];
+        if (Object.keys(ag.workspaces).length > 0) {
+          Object.keys(ag.workspaces).forEach(function(w) {
+            let envlinks = [];
+            let wks = ag.workspaces[w];
+            let wksName = wks.workspace.value.replace(/\//g, "_"); //Replace forward slash by underscore
+            dot.push("\t\tsubgraph cluster_" + wksName + " {\n");
+            dot.push("\t\t\tlabel=\"" + wks.workspace.value + "\" labeljust=\"r\" style=dashed pencolor=gray40 fontcolor=gray40\n");
+            if (wks.artifacts != null) Object.keys(wks.artifacts).forEach(function(a) {
+              if (p.HIDDEN_ARTS.indexOf(wks.artifacts[a].type.value) < 0) {
+                s1 = (wks.artifacts[a].artifact.value.length <= p.MAX_LENGTH) ? 
+                  wks.artifacts[a].artifact.value :
+                  wks.artifacts[a].artifact.value.substring(0, p.MAX_LENGTH) + " ...";
+                dot.push("\t\t\t\"" + wksName + "_" + wks.artifacts[a].artifact.value + "\" [label = \"" + s1 + ":\\n");
+                s1 = (wks.artifacts[a].type.value.length <= p.MAX_LENGTH) ? 
+                  wks.artifacts[a].type.value :
+                  wks.artifacts[a].type.value.substring(0, p.MAX_LENGTH) + " ...";
+                dot.push(s1 + "\"shape=record style=filled fillcolor=white];\n");
+                wksartifacts.push(wksName + "_" + wks.artifacts[a].artifact.value);
+                envlinks.push("\t\t\"" + ag.agent.value + "\"->\"" + wksName + "_" + wks.artifacts[a].artifact.value + "\" [arrowhead=odot]\n");
+              }
+            });
+  
+            dot.push("\t\t}\n");
+            dot.push(envlinks.join(" "));
+          });
+        }
+      });
+      dot.push("\t}\n");
+  
+      dot.push("}\n");  
+    }
+
+    let graph = dot.join("");
+    //console.log(graph);
+    if (graph !== masGraphCache) {
+      masGraphCache = graph;
+      /* Transition follows modal top down movement */
+      import( /* webpackChunkName: "d3" */ 'd3').then(function(d3) {
+        import( /* webpackChunkName: "d3-graphviz" */ 'd3-graphviz').then(function(d3G) {
+          var t = d3.transition().duration(500).ease(d3.easeLinear);
+          d3G.graphviz("#overviewgraph").transition(t).renderDot(masGraphCache);
+        });
+      });
+    }
+
   });
 }
 
